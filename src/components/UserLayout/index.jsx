987ed6c477990999bc4301f48d9171662827aff1
Tal Ben Adon <talbenadon2@gmail.com>
2024-02-02 04:48:32 +0200

    refined code

--- src/components/UserLayout/index.jsx
+++ src/components/UserLayout/index.jsx
@@ -5,7 +5,7 @@ export default function UserLayout() {
     return (
         <div className={styles.userLayoutContainer}>
 
-            <div className={styles.topTitle}>What do you have to do?</div>
+
             <MissionTable />
         </div>
 
